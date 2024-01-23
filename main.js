87ee6eb9b87f320a34ee830089039e1df7407c9e
yujin1018 <115362555+yujin1018@users.noreply.github.com>
2024-01-24 01:45:11 +0900

    js header

---
+++ main.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    if(window.scrollY >headerHeight){
+        header.classList.add('header--dark');
+    }else{
+        header.classList.remove('header--dark')
+    }
+})
