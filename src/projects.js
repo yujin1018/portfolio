41c7acce14231591885f1111f8561359240ba3f9
yujin1018 <115362555+yujin1018@users.noreply.github.com>
2024-01-28 00:43:13 +0900

    project filter

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,21 @@
 'use strict'
+
+// according to project filtering logic
+
+
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+categories.addEventListener('click',(event)=>{
+    const filter = event.target.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    projects.forEach(project=>{
+     if(filter===project.dataset.type){
+        project.style.display='block';
+     }else{
+        project.style.display='none';
+     }
+    })
+})
+
