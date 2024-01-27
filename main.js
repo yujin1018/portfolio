1553e79cb1eec1811a4db48034766df9f41e1942
yujin1018 <115362555+yujin1018@users.noreply.github.com>
2024-01-27 23:18:58 +0900

    arrow-up btn

--- main.js
+++ main.js
@@ -14,16 +14,17 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', ()=>{
     home.style.opacity= 1-window.scrollY/homeHeight;
-    console.log(home.style.opacity= 1-window.scrollY/homeHeight)
 })
 
-// const home = document.querySelector('#home');
-// const homeHeight = home.getBoundingClientRect().height;
-// document.addEventListener('scroll',()=>{
-//     if(window.scrollY>homeHeight){
-//         home.classList.add('home--clear')
-//     }else{
-//         home.classList.remove('home--dark')
-//     }
-// })
+// Arrow up 버튼을 아래로 스크롤시 투명하게 처리
+
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll',()=>{
+    if(window.scrollY>homeHeight/2){
+        arrowUp.style.opacity =1;
+    }else{
+        arrowUp.style.opacity =0;
+    }
+})
+
 
