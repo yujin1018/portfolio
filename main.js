7530d25f01a530f4326a4d633429bcd506bcc191
yujin1018 <115362555+yujin1018@users.noreply.github.com>
2024-01-27 23:53:01 +0900

    responsive js

--- main.js
+++ main.js
@@ -33,3 +33,9 @@ const navbarToggle = document.querySelector('.header__toggle');
 navbarToggle.addEventListener('click',()=>{
     navbarMenu.classList.toggle('open');
 })
+
+
+// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌.
+navbarMenu.addEventListener('click',()=>{
+    navbarMenu.classList.remove('open');
+})
