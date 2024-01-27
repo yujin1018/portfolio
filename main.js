d946c60a74f96afe8f1bfdb0d96377b8027e69aa
yujin1018 <115362555+yujin1018@users.noreply.github.com>
2024-01-27 23:02:06 +0900

    js-home opacity

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ document.addEventListener('scroll',()=>{
         header.classList.remove('header--dark')
     }
 })
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함!
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', ()=>{
+    home.style.opacity= 1-window.scrollY/homeHeight;
+    console.log(home.style.opacity= 1-window.scrollY/homeHeight)
+})
+
+// const home = document.querySelector('#home');
+// const homeHeight = home.getBoundingClientRect().height;
+// document.addEventListener('scroll',()=>{
+//     if(window.scrollY>homeHeight){
+//         home.classList.add('home--clear')
+//     }else{
+//         home.classList.remove('home--dark')
+//     }
+// })
+
