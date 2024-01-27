dda6b043db1b127ea71f100849c1f65334ad3fa1
yujin1018 <115362555+yujin1018@users.noreply.github.com>
2024-01-28 01:26:58 +0900

    refectoring

--- src/projects.js
+++ src/projects.js
@@ -5,17 +5,41 @@
 
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects')
 categories.addEventListener('click',(event)=>{
     const filter = event.target.dataset.filter;
     if(filter == null){
         return;
     }
-    projects.forEach(project=>{
-     if(filter===project.dataset.type){
-        project.style.display='block';
-     }else{
-        project.style.display='none';
-     }
-    })
-})
+    handleActiveSelection(event.target);
+    filterProjects(filter);
+});
 
+    
+
+
+
+
+    // Project filtering
+
+
+function handleActiveSelection(target){
+const active = document.querySelector('.category--selected');
+active.classList.remove('category--selected')
+target.classList.add('category--selected');
+}
+
+function filterProjects(filter){
+    projects.forEach(project=>{
+        if(filter===project.dataset.type){
+           project.style.display='block';
+        }else{
+           project.style.display='none';
+        }
+       });
+       projectsContainer.classList.add('anim-out');
+       setTimeout(()=>{
+           projectsContainer.classList.remove('anim-out');
+       },250)
+   }
+   
